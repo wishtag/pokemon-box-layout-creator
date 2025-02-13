d465a66f9b52343563bd6b16e6b914af5ef7736b
HaydenPhillips777 <hlphillips1@ualr.edu>
2025-02-13 14:25:01 -0600

    Actual Release

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # pokemon-box-layout-creator
 Python scripts to create and manage your Pokémon box layouts
+
+    // If set to true, then pokemon in the shiny_locked.txt file will be marked out
+    // If set to false, shiny_locked.txt is ignored
+    "shiny_dex": false,
+
+    // If set to true, box.py and box_caught.py will open the text file after saving to it
+    // If set to false, box.py and box_caught.py will not open the text file after saving to it
+    "open_when_done": true,
+
+    // If set to true, box_caught.py will instead mark out all the pokemon you haven't caught
+    // If set to false, box_caught.py will behave as usual and mark out all the pokemon you have caught
+    "invert_caught": false
--- python/box.py
+++ python/box.py
@@ -1,77 +1,31 @@
 import math
 import os
+import json
+
+with open(os.path.abspath('./settings.json'), 'r') as file:
+    settings = json.loads(file.read())
 
 max_length = 19
 names = []
-caught_names = []
-not_evolved_names = []
-shiny_locked_names = []
 pokemon_index = 0
 empty = " "
 empty = f"[{empty.center(max_length)}] "
-caught = "▓"
-caught = f"[{caught.center(max_length, caught)}] "
-s_locked = "▒"
-s_locked = f"[{s_locked.center(max_length, s_locked)}] "
 
-with open('mons.txt', 'r') as file:
+with open(os.path.abspath('./text/mons.txt'), 'r') as file:
     for line in file:
         name = f"[{line.strip().center(max_length)}] "
         names.append(name)
-
-
-with open('caught.txt', 'r') as file:
-    for line in file:
-        caught_name = f"[{line.strip().center(max_length)}] "
-        caught_names.append(caught_name)
-
-#"""
-for i in range(len(caught_names)):
-    for j in range(len(names)):
-        if caught_names[i] == names[j]:
-            names[j] = caught
-            break
-#"""
-"""
-for i in range(len(caught_names)):
-    for j in range(len(names)):
-        if names[j] not in caught_names:
-            names[j] = caught
-"""
-
-with open('caught_but_not_evolved.txt', 'r') as file:
-    for line in file:
-        not_evolved_name = f"[{line.strip().center(max_length)}] "
-        not_evolved_names.append(not_evolved_name)
-
-for i in range(len(not_evolved_names)):
-    for j in range(len(names)):
-        if not_evolved_names[i] == names[j]:
-            names[j] = names[j].replace("[",">").replace("]","<")
-            break
-
-
-with open('shiny_locked.txt', 'r') as file:
-    for line in file:
-        shiny_locked_name = f"[{line.strip().center(max_length)}] "
-        shiny_locked_names.append(shiny_locked_name)
-
-for i in range(len(shiny_locked_names)):
-    for j in range(len(names)):
-        if shiny_locked_names[i] == names[j]:
-            names[j] = s_locked
-            break
             
-f = open("formatted_caught.txt", "w")
+f = open(os.path.abspath('./text/box.txt'), 'w')
 f.write("")
 f.close()
 
 total_boxes = math.ceil(float(len(names)/30))
 
-f = open("formatted_caught.txt", "a", encoding='utf-8')
+f = open(os.path.abspath('./text/box.txt'), 'a', encoding='utf-8')
 for current_box in range(1,total_boxes+1):
 
-    # max_length is how many characters a pokemons name is
+    # max_length is how many characters a pokemons name can be
     # the "+2" is to represent the two brackets, [], at the start and end of a pokemons name
     # the "*6" is to represent how many pokemon can be in one row
     # the "+7" is the additional spaces in between the start and end of each pokemons name
@@ -112,5 +66,5 @@ for current_box in range(1,total_boxes+1):
         f.write("─")
     f.write("┘\n")
 
-#time.sleep(1)
-os.startfile("formatted_caught.txt")
+if settings["open_when_done"]:
+    os.startfile(os.path.abspath('./text/box.txt'))
